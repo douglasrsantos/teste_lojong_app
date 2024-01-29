37396fd70aa6c84a0ec00c062430d22147ec4841
Douglas Santos <douglasrosasantos@gmail.com>
2024-01-29 19:07:08 -0300

    comment for tests

--- lib/app/core/services/initialize_dio.dart
+++ lib/app/core/services/initialize_dio.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 import 'package:chucker_flutter/chucker_flutter.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
-import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
-import 'package:teste_lojong/app/core/app_constants.dart';
+// import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+// import 'package:teste_lojong/app/core/app_constants.dart';
 import 'package:teste_lojong/app/core/app_core.dart';
-import 'package:teste_lojong/app/core/services/cache_interceptor_path.dart';
+// import 'package:teste_lojong/app/core/services/cache_interceptor_path.dart';
 
 class InitializeDio {
   static initializeDioOptions(Dio client, String hiveBoxName) async {
@@ -19,19 +19,21 @@ class InitializeDio {
 
     client.interceptors.add(ChuckerDioInterceptor());
 
-    client.interceptors.add(
-      DioCacheInterceptor(
-        options: CacheOptions(
-          store: await CacheInterceptorPath().configCacheStore(hiveBoxName),
-          policy: CachePolicy.refreshForceCache,
-          hitCacheOnErrorExcept: [],
-          maxStale: const Duration(
-            days: AppConstants.cacheDays,
-          ),
-          priority: CachePriority.high,
-        ),
-      ),
-    );
+    // commented for tests
+    // 
+    // client.interceptors.add(
+    //   DioCacheInterceptor(
+    //     options: CacheOptions(
+    //       store: await CacheInterceptorPath().configCacheStore(hiveBoxName),
+    //       policy: CachePolicy.refreshForceCache,
+    //       hitCacheOnErrorExcept: [],
+    //       maxStale: const Duration(
+    //         days: AppConstants.cacheDays,
+    //       ),
+    //       priority: CachePriority.high,
+    //     ),
+    //   ),
+    // );
 
     client.httpClientAdapter = IOHttpClientAdapter(
       createHttpClient: () {
